ac5086c2bf7ad89fa565a077bc5bd5a842040892
Lee Minhoon <lmh_1220@naver.com>
2020-10-27 23:33:23 +0900

    init commit

---
+++ Midterm Exam 1.R
@@ -0,0 +1,43 @@
+# Call Library
+library(magick)
+
+# Set Variable
+pool_path = "./resource/pool.png"
+pool <- image_read(pool_path)
+pool <- image_scale(pool, "300X400!")
+
+ball_path = "./resource/ball.png"
+ball <- image_read(ball_path)
+ball <- image_scale(ball, "40X40!")
+
+x <- 0
+y <- 0
+xd <- TRUE
+yd <- TRUE
+
+while(TRUE)
+{
+  # X, Y의 위치 (공의 위치)
+  position <- paste("+", x, "+", y, sep = "")
+  
+  # 당구대이미지에 당구공이미지를 offset위치로 합침
+  img <- image_composite(pool, ball, offset = position)
+  
+  # 이미지를 출력
+  print(img)
+  
+  # x와 y가 움직히는 방향을 정함
+  if (x == 260) xd = FALSE
+  else if (x == 0) xd = TRUE
+  if (y == 360) yd = FALSE
+  else if (y == 0) yd = TRUE
+  
+  # x, y값을 바꿔줌
+  if (xd) x = x + 10
+  else x = x - 10
+  if (yd) y = y + 10
+  else y = y - 10
+  
+  # 대기
+  Sys.sleep(0.1)
+}
